22c38fccec55ec0fbe26d8d72f2a9af21db0d96f
oggy-learn-code <vunguyenhuychuong10t2@gmail.com>
2022-07-18 00:37:36 +0700

    Complete DAO,Repository 1.Member,2.Product,3.Order,4.OrderDetail

---
+++ eStoreWebApp/DataAccess/Repository/IMemberRepository.cs
@@ -0,0 +1,28 @@
+﻿using BussinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IMemberRepository
+    {
+        IEnumerable<Member> GetMembers();
+
+        IEnumerable<Member> GetMemberByIDList(int memid);
+
+        Member GetMemberByID(int memId);
+
+        Member GetMemberByName(string memName);
+
+        void InsertMember(Member member);
+
+        void DeleteMember(int memId);
+
+        void UpdateMember(Member member);
+
+        List<Member> GetMemberByCityAndCountry(string text1, string text2);
+    }
+}
---
+++ eStoreWebApp/DataAccess/Repository/IOrderDetailRepository.cs
@@ -0,0 +1,30 @@
+﻿using BussinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IOrderDetailRepository
+    {
+        IEnumerable<OrderDetail> GetOrderDetails();
+
+        IEnumerable<OrderDetail> GetOrderDetailListByMemberID(int id);
+
+        OrderDetail GetOrderDetailByID(int OrderID, int ProductID);
+
+        void InsertOrderDetail(OrderDetail OrderDetail);
+
+        void DeleteOrderDetail(int OrderID, int ProductID);
+
+        void UpdateOrderDetail(OrderDetail OrderDetail);
+
+        public double GetStatistic(IEnumerable<Order> id);
+
+        public IEnumerable<OrderDetail> GetOrderDetailListByListOrder(IEnumerable<Order> id);
+
+        public IEnumerable<OrderDetail> GetOrderDetailListByListOrder(List<Order> id);
+    }
+}
